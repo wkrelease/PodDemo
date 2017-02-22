08439e13e5e9e8aeca7ab8d5c3a96ca6862a8d21
王坤成 <kuncheng_wang@talkmate.com>
2017-02-22 10:37:20 +0800

    11
    11

---
+++ PodDemo/PodDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodDemo
+//
+//  Created by king on 2017/2/22.
+//  Copyright © 2017年 KC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PodDemo/PodDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PodDemo
+//
+//  Created by king on 2017/2/22.
+//  Copyright © 2017年 KC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
